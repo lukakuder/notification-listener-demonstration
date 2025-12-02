55c7d8e1a97ecea8a066449a8bcb94650008e459
kuderluka <luka.kuder@gmail.com>
2025-12-02 18:17:55 +0100

    ViewBinding

--- app/src/main/java/kuderl/si/notification_listener_demonstration/MainActivity.kt
+++ app/src/main/java/kuderl/si/notification_listener_demonstration/MainActivity.kt
@@ -3,16 +3,16 @@ package kuderl.si.notification_listener_demonstration
 import android.content.Intent
 import android.os.Bundle
 import android.provider.Settings
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import kuderl.si.notification_listener_demonstration.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
-        val openSettings = findViewById<Button?>(R.id.btn_open_settings)
-        openSettings.setOnClickListener {
+        binding.settingsButton.setOnClickListener {
             val intent = Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS)
             startActivity(intent)
         }
